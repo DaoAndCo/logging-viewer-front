5b353d647293d5c0d746213725e3c18ad027db1c
Flavien Beninca <flavien.beninca@gmail.com>
2016-05-24 15:08:28 +0200

    Object config

---
+++ src/config/app.default.js
@@ -0,0 +1,7 @@
+export const Config = Object.freeze({
+  api: {
+    baseURL: 'http://api.logs.com/',
+    username: 'admin',
+    password: 'my-api-key'
+  }
+})
--- src/store/logs/actions.js
+++ src/store/logs/actions.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { Config } from 'src/config/app.js'
 
 var api = axios.create({
-  baseURL: 'http://mooc.local/logging-viewer-api/',
+  baseURL: Config.api.baseURL,
   auth: {
-    username: 'admin',
-    password: 'ad40db557bf3dd377243348a24ed215702b1c596'
+    username: Config.api.username,
+    password: Config.api.password
   }
 })
 
@@ -21,16 +22,3 @@ export const loadLogs = function (store, page) {
       console.log(response)
     })
 }
-
-// export const getLogs = function (store) {
-//   return api
-//     .post('find', {config: 'logs', limit: 500, order: {created: 'DESC'}})
-//     .then((response) => {
-//       store.dispatch('ADD_LOGS', response.data.logs)
-//       store.dispatch('EXTRACT_SCOPES')
-//       store.dispatch('EXTRACT_LEVELS')
-//     })
-//     .catch((response) => {
-//       console.log(response)
-//     })
-// }
